e551940ce6067eddeb24da03cd699dcb8ad1846a
jonatan <jonatan@jslsolucoes.com>
2016-11-24 03:43:34 -0200

    ...

--- nginx-admin-ui/src/main/java/com/jslsolucoes/nginx/admin/nginx/CentOsRunner.java
+++ nginx-admin-ui/src/main/java/com/jslsolucoes/nginx/admin/nginx/CentOsRunner.java
@@ -48,7 +48,7 @@ public class CentOsRunner implements Runner {
 
 	@Override
 	public RuntimeResult status() {
-		return RuntimeUtils.command("ps -p $("+nginx.getPid()+")");
+		return RuntimeUtils.command("ps -p $(cat "+nginx.getPid()+")");
 	}
 
 	@Override
