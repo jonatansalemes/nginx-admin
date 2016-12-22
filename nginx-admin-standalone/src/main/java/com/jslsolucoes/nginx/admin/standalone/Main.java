4de6e07e6c19d4bbeb49d3edf3a83fa7d4f4255d
jslsolucoes <jonatan@jslsolucoes.com>
2016-12-22 05:39:48 -0200

    Prepare for next develop iteration

--- nginx-admin-standalone/src/main/java/com/jslsolucoes/nginx/admin/standalone/Main.java
+++ nginx-admin-standalone/src/main/java/com/jslsolucoes/nginx/admin/standalone/Main.java
@@ -37,7 +37,7 @@ public class Main {
 					.rootLogger(Level.ERROR));
 			swarm.start();
 
-			InputStream war = Main.class.getResourceAsStream("/nginx-admin-ui-1.0.2.war");
+			InputStream war = Main.class.getResourceAsStream("/nginx-admin-ui-1.0.3.war");
 			File file = File.createTempFile(UUID.randomUUID().toString(), ".war");
 			FileUtils.copyInputStreamToFile(war, file);
 			file.deleteOnExit();
