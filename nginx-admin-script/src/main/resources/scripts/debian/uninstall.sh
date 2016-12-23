0d8fb8a20f683d07570178b5249670bfa203153d
jslsolucoes <jonatan@jslsolucoes.com>
2016-12-23 04:36:34 -0200

    Scripts evolution

---
+++ nginx-admin-script/src/main/resources/scripts/debian/uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+service nginx-admin stop
+rm -rf /opt/nginx-admin
+rm -rf /etc/init.d/nginx-admin
+userdel nginx-admin
+userdel nginx
+sed -i 's/nginx-admin ALL = NOPASSWD: \/usr\/sbin\/nginx,\/usr\/bin\/pgrep//g' /etc/sudoers
+sed -i 's/Defaults:nginx-admin !requiretty//g' /etc/sudoers
