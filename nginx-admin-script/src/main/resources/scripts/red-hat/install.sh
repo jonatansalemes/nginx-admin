eeadbafb0c0c5443f24e547bbee6a927b74243f2
JSL Solucoes LTDA <jonatan@jslsolucoes.com>
2017-02-15 19:02:51 -0200

    Update install.sh
    Correction in url for service

--- nginx-admin-script/src/main/resources/scripts/red-hat/install.sh
+++ nginx-admin-script/src/main/resources/scripts/red-hat/install.sh
@@ -90,7 +90,7 @@ printf 'nginx-admin ALL = NOPASSWD: /usr/sbin/nginx,/usr/bin/pgrep\n' >> /etc/su
 printf 'Defaults:nginx-admin !requiretty\n' >> /etc/sudoers
 
 if ! file_exists "/etc/init.d/nginx-admin" ; then 
-	wget https://raw.githubusercontent.com/jslsolucoes/nginx-admin/master/nginx-admin-script/src/main/resources/install/red-hat/nginx-admin.sh -O /etc/init.d/nginx-admin
+	wget https://raw.githubusercontent.com/jslsolucoes/nginx-admin/master/nginx-admin-script/src/main/resources/scripts/red-hat/nginx-admin.sh -O /etc/init.d/nginx-admin
 	chmod +x /etc/init.d/nginx-admin
 	chown root:root /etc/init.d/nginx-admin
 	chkconfig --level 345 nginx-admin on
