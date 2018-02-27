4bcac3a25d7ed41e05fbf053d0cd527d1af00347
jonatanlemes <jonatanlemes@localhost>
2018-02-27 01:10:38 -0300

    Prepare for version 2.0.2 next iteration development. Normalize agent names

--- nginx-admin-docker/release/nginx-admin-agent/debian/build/install.sh
+++ nginx-admin-docker/release/nginx-admin-agent/debian/build/install.sh
@@ -22,30 +22,30 @@ chmod 440 /etc/sudoers
 
 #download and extract latest version of nginx agent package
 mkdir -p /opt/downloads
-wget https://bintray.com/jslsolucoes/nginx-admin/download_file?file_path=nginx-agent-2.0.1.zip -O /opt/downloads/nginx-agent-2.0.1.zip
-unzip /opt/downloads/nginx-agent-2.0.1.zip -d /opt
-chmod -R 755 /opt/nginx-agent-2.0.1
-chown -R nginx-agent:nginx-agent /opt/nginx-agent-2.0.1
+wget https://bintray.com/jslsolucoes/nginx-admin/download_file?file_path=nginx-admin-agent-2.0.2.zip -O /opt/downloads/nginx-admin-agent-2.0.2.zip
+unzip /opt/downloads/nginx-admin-agent-2.0.2.zip -d /opt
+chmod -R 755 /opt/nginx-admin-agent-2.0.2
+chown -R nginx-agent:nginx-agent /opt/nginx-admin-agent-2.0.2
 
 #set environment variable
-printf 'NGINX_AGENT_HOME=/opt/nginx-agent-2.0.1\n' >> /etc/environment
+printf 'NGINX_AGENT_HOME=/opt/nginx-admin-agent-2.0.2\n' >> /etc/environment
 
 #add init scripts to os
-cp /opt/nginx-agent-2.0.1/scripts/debian/nginx-agent.sh /etc/init.d/nginx-agent
-chmod +x /etc/init.d/nginx-agent
-chown root:root /etc/init.d/nginx-agent
-update-rc.d nginx-agent defaults
-update-rc.d nginx-agent enable
+cp /opt/nginx-admin-agent-2.0.2/scripts/debian/nginx-admin-agent.sh /etc/init.d/nginx-admin-agent
+chmod +x /etc/init.d/nginx-admin-agent
+chown root:root /etc/init.d/nginx-admin-agent
+update-rc.d nginx-admin-agent defaults
+update-rc.d nginx-admin-agent enable
 
 #start service
-service nginx-agent start
+service nginx-admin-agent start
 
 #logs from server
-tail -f /opt/nginx-agent-2.0.1/log/console.log
+tail -f /opt/nginx-admin-agent-2.0.2/log/console.log
 
 #you can check for connectivity after a few second that must return http status 200 OK for the request
 wget --header "Authorization: changeit" http://localhost:3000
 
 #By default your authorization key is "changeit" you also can check for connectivity in your browser accessing http://ip:3000 or https://ip:3443 that will return http status 403 forbidden with message "Resource forbidden" because requires authorization header to work
-#Please access /opt/nginx-agent-2.0.1/conf/nginx-agent.conf and change variable NGINX_AGENT_AUTHORIZATION_KEY=changeit value for one that only you knows and unique for every node that you have installed. You can also change others configurations if you like in this file and restart service to apply new settings
+#Please access /opt/nginx-admin-agent-2.0.2/conf/nginx-admin-agent.conf and change variable NGINX_ADMIN_AGENT_AUTHORIZATION_KEY=changeit value for one that only you knows and unique for every node that you have installed. You can also change others configurations if you like in this file and restart service to apply new settings
 #Nginx manager ui will ask for this authorization key value to connect with this agent.
--- nginx-admin-docker/release/nginx-admin-agent/red-hat/build/install.sh
+++ nginx-admin-docker/release/nginx-admin-agent/red-hat/build/install.sh
@@ -22,36 +22,36 @@ chmod 440 /etc/sudoers
 
 #download and extract latest version of nginx agent package
 mkdir -p /opt/downloads
-wget https://bintray.com/jslsolucoes/nginx-admin/download_file?file_path=nginx-agent-2.0.1.zip -O /opt/downloads/nginx-agent-2.0.1.zip
-unzip /opt/downloads/nginx-agent-2.0.1.zip -d /opt
-chmod -R 755 /opt/nginx-agent-2.0.1
-chown -R nginx-agent:nginx-agent /opt/nginx-agent-2.0.1
+wget https://bintray.com/jslsolucoes/nginx-admin/download_file?file_path=nginx-admin-agent-2.0.2.zip -O /opt/downloads/nginx-admin-agent-2.0.2.zip
+unzip /opt/downloads/nginx-admin-agent-2.0.2.zip -d /opt
+chmod -R 755 /opt/nginx-admin-agent-2.0.2
+chown -R nginx-agent:nginx-agent /opt/nginx-admin-agent-2.0.2
 
 #set environment variable
-printf 'NGINX_AGENT_HOME=/opt/nginx-agent-2.0.1\n' >> /etc/environment
+printf 'NGINX_AGENT_HOME=/opt/nginx-admin-agent-2.0.2\n' >> /etc/environment
 
 #add init scripts to os
-cp /opt/nginx-agent-2.0.1/scripts/red-hat/nginx-agent.sh /etc/init.d/nginx-agent
-chmod +x /etc/init.d/nginx-agent
-chown root:root /etc/init.d/nginx-agent
-chkconfig --level 345 nginx-agent on
+cp /opt/nginx-admin-agent-2.0.2/scripts/red-hat/nginx-admin-agent.sh /etc/init.d/nginx-admin-agent
+chmod +x /etc/init.d/nginx-admin-agent
+chown root:root /etc/init.d/nginx-admin-agent
+chkconfig --level 345 nginx-admin-agent on
 
-#release ports on firewalld to nginx-agent and to nginx	
-printf '&lt;?xml version="1.0" encoding="utf-8"?&gt;\n&lt;service&gt;\n&lt;short&gt;Nginx agent firewall service&lt;/short&gt;\n&lt;description&gt;Nginx agent firewall service&lt;/description&gt;\n&lt;port protocol="tcp" port="3000"/&gt;\n&lt;port protocol="tcp" port="3443"/&gt;\n&lt;/service&gt;\n' &gt;&gt; /etc/firewalld/services/nginx-agent.xml
-firewall-cmd --zone=public --add-service=nginx-agent --permanent
+#release ports on firewalld to nginx-admin-agent and to nginx	
+printf '&lt;?xml version="1.0" encoding="utf-8"?&gt;\n&lt;service&gt;\n&lt;short&gt;Nginx agent firewall service&lt;/short&gt;\n&lt;description&gt;Nginx agent firewall service&lt;/description&gt;\n&lt;port protocol="tcp" port="3000"/&gt;\n&lt;port protocol="tcp" port="3443"/&gt;\n&lt;/service&gt;\n' &gt;&gt; /etc/firewalld/services/nginx-admin-agent.xml
+firewall-cmd --zone=public --add-service=nginx-admin-agent --permanent
 firewall-cmd --zone=public --add-service=http --permanent
 firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --reload
 
 #start service
-service nginx-agent start
+service nginx-admin-agent start
 
 #logs from server
-tail -f /opt/nginx-agent-2.0.1/log/console.log
+tail -f /opt/nginx-admin-agent-2.0.2/log/console.log
 
 #you can check for connectivity after a few seconds that must return http status 200 OK for the request
 wget --header "Authorization: changeit" http://localhost:3000
 
 #By default your authorization key is "changeit" you also can check for connectivity in your browser accessing http://ip:3000 or https://ip:3443 that will return http status 403 forbidden with message "Resource forbidden" because requires authorization header to work
-#Please access /opt/nginx-agent-2.0.1/conf/nginx-agent.conf and change variable NGINX_AGENT_AUTHORIZATION_KEY=changeit value for one that only you knows and unique for every node that you have installed. You can also change others configurations if you like in this file and restart service to apply new settings
+#Please access /opt/nginx-admin-agent-2.0.2/conf/nginx-admin-agent.conf and change variable NGINX_AGENT_AUTHORIZATION_KEY=changeit value for one that only you knows and unique for every node that you have installed. You can also change others configurations if you like in this file and restart service to apply new settings
 #Nginx manager ui will ask for this authorization key value to connect with this agent.
